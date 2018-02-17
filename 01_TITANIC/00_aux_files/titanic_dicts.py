c9160cff358797e58de1662dc54c496611dbd414
Cristina Serrano <cserrano@gmx.com>
2018-02-17 12:40:31 +0100

    class_20180217: titanic example and pwpnt

---
+++ 01_TITANIC/00_aux_files/titanic_dicts.py
@@ -0,0 +1,16 @@
+dict_survival = {
+    0: 'No',
+    1: 'Yes'
+}
+
+dict_ticket_class = {
+    1: '1st',
+    2: '2nd',
+    3: '3rd'
+}
+
+dict_embarked_port = {
+    'C': 'Cherbourg',
+    'Q': 'Queenstown', 
+    'S': 'Southampton'
+}
